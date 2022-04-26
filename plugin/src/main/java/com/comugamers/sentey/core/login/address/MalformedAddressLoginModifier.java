622b7c3243b3e55c6aa0595b639b8cf00b36709f
Jojo1542 <admin@jojo1542.es>
2022-04-26 01:11:43 -0300

    fix(login): fix incorrect modifier results

--- plugin/src/main/java/com/comugamers/sentey/core/login/address/MalformedAddressLoginModifier.java
+++ plugin/src/main/java/com/comugamers/sentey/core/login/address/MalformedAddressLoginModifier.java
@@ -22,9 +22,14 @@ public class MalformedAddressLoginModifier implements LoginModifier {
         // Check if we should check for malformed IP addresses - usually this is already done by
         // the null address check, but we do it again just in case
         if(config.getBoolean("config.login.block-invalid-addresses.enabled")) {
+            // Get the spoofed address as a string
+            String spoofedAddress = context.getSpoofedAddress().getHostAddress();
+
+            // Get the handshake address as a string as well
+            String handshakeAddress = context.getHandshakeAddress().getHostAddress();
+
             // Return false if any of the IP addresses is malformed
-            return !isValidIPv4(context.getHandshakeAddress().getHostAddress())
-                    || !isValidIPv4(context.getSpoofedAddress().getHostAddress());
+            return isValidIPv4(spoofedAddress) && isValidIPv4(handshakeAddress);
         }
 
         // If all conditions above failed, allow the login attempt
