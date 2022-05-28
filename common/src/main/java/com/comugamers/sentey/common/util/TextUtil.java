23da08930c96a79fef4698ca0f8d7d0afb036886
Pabszito <pabszito@gmail.com>
2022-05-27 23:19:46 -0300

    docs(common): add javadocs to multiple classes

--- common/src/main/java/com/comugamers/sentey/common/util/TextUtil.java
+++ common/src/main/java/com/comugamers/sentey/common/util/TextUtil.java
@@ -6,15 +6,22 @@ import java.util.List;
 
 public class TextUtil {
 
+    /**
+     * Colorizes a {@link String} by using {@link ChatColor#translateAlternateColorCodes(char, String)}.
+     * @param str The {@link String} to colorize.
+     * @return The colorized {@link String}.
+     */
     public static String colorize(String str) {
         return ChatColor.translateAlternateColorCodes('&', str);
     }
 
+    /**
+     * Colorizes a {@link List<String>} by using {@link ChatColor#translateAlternateColorCodes(char, String)}.
+     * @param list The {@link List<String>} to colorize
+     * @return The colorized {@link List<String>}
+     */
     public static List<String> colorize(List<String> list) {
-        for(int i = 0; i < list.size(); i++) {
-            list.set(i, colorize(list.get(i)));
-        }
-
+        list.replaceAll(TextUtil::colorize);
         return list;
     }
 }
