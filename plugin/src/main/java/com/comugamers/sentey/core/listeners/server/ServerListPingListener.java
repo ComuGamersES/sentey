4cf256f8b9c4896fd78514936e1940a46a3db1e4
Jojo1542 <admin@jojo1542.es>
2022-05-28 01:37:02 -0300

    fix(ping): check if server list ping listening is enabled

--- plugin/src/main/java/com/comugamers/sentey/core/listeners/server/ServerListPingListener.java
+++ plugin/src/main/java/com/comugamers/sentey/core/listeners/server/ServerListPingListener.java
@@ -1,5 +1,6 @@
 package com.comugamers.sentey.core.listeners.server;
 
+import com.comugamers.sentey.common.file.YamlFile;
 import com.comugamers.sentey.core.Sentey;
 import com.comugamers.sentey.core.ping.action.PingAction;
 import com.comugamers.sentey.core.ping.filter.PingFilter;
@@ -15,28 +16,37 @@ public class ServerListPingListener implements Listener {
     @Inject
     private Sentey plugin;
 
+    @Inject
+    private YamlFile config;
+
     @EventHandler
     public void onServerListPingEvent(ServerListPingEvent event) {
         // Get the source address
         InetAddress address = event.getAddress();
 
+        // Check if we should ignore the event
+        if (!config.getBoolean("config.server-list-ping.enabled")) {
+            // If so, return
+            return;
+        }
+
         // Check if the address is null
-        if(address == null) {
+        if (address == null) {
             // If so, return
             return;
         }
 
         // Loop through each registered filter
-        for(PingFilter filter : plugin.getPingFilters()) {
+        for (PingFilter filter : plugin.getPingFilters()) {
             // Check if the ping should be ignored
-            if(!filter.isClean(address)) {
+            if (!filter.isClean(address)) {
                 // If so, return
                 return;
             }
         }
 
         // Loop through each registered ping action
-        for(PingAction action : plugin.getPingActions()) {
+        for (PingAction action : plugin.getPingActions()) {
             // And execute it
             action.handle(address);
         }
