2b9f51ede6066ee1c7b40a500ed7df0fda0f5e86
Pabszito <pabszito@gmail.com>
2022-06-24 19:33:41 -0300

    feat(login): add option for logging socket addresses

--- src/main/java/com/comugamers/sentey/listeners/player/PlayerLoginListener.java
+++ src/main/java/com/comugamers/sentey/listeners/player/PlayerLoginListener.java
@@ -4,21 +4,40 @@ import com.comugamers.sentey.login.filter.LoginFilter;
 import com.comugamers.sentey.login.action.LoginAction;
 import com.comugamers.sentey.login.structure.LoginContext;
 import com.comugamers.sentey.Sentey;
+import com.comugamers.sentey.util.ConnectionUtil;
+import com.comugamers.sentey.util.file.YamlFile;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 import javax.inject.Inject;
 
+import static com.comugamers.sentey.util.ConnectionUtil.getRemoteAddress;
+
 public class PlayerLoginListener implements Listener {
 
     @Inject
     private Sentey plugin;
 
+    @Inject
+    private YamlFile config;
+
     @EventHandler
     public void onPlayerLoginEvent(PlayerLoginEvent event) {
+        // Get the player who logged in
+        Player player = event.getPlayer();
+
         // Create a new login context
         LoginContext ctx = new LoginContext(event);
+
+        // Check if we should log socket addresses
+        if(config.getBoolean("config.log-socket-addresses", false)) {
+            // If so, do it
+            plugin.getLogger().info(
+                    "Player " + player.getName() + " is joining through " + getRemoteAddress(player)
+            );
+        }
 
         // Loop through each login filter
         for(LoginFilter module : plugin.getLoginFilters()) {
