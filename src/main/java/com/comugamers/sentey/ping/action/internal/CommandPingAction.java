35c16410fc9fca48b4c53157313930b9cde07bea
Jojo1542 <admin@jojo1542.es>
2022-06-24 18:04:17 -0300

    feat(ping): add command ping action

---
+++ src/main/java/com/comugamers/sentey/ping/action/internal/CommandPingAction.java
@@ -0,0 +1,36 @@
+package com.comugamers.sentey.ping.action.internal;
+
+import com.comugamers.sentey.Sentey;
+import com.comugamers.sentey.ping.action.PingAction;
+import com.comugamers.sentey.util.PlaceholderUtil;
+import com.comugamers.sentey.util.file.YamlFile;
+
+import javax.inject.Inject;
+import java.net.InetAddress;
+
+public class CommandPingAction implements PingAction {
+
+    @Inject
+    private YamlFile config;
+
+    @Inject
+    private Sentey plugin;
+
+    @Override
+    public void handle(InetAddress address) {
+        // Check if we should run commands
+        if(config.getBoolean("config.server-list-ping.actions.commands.enabled")) {
+            // If so, loop through the list of commands
+            config.getStringList("config.login.actions.commands.list").forEach(
+                    // And dispatch each one of them
+                    command ->
+                            plugin.getServer().dispatchCommand(
+                                    plugin.getServer().getConsoleSender(),
+                                    PlaceholderUtil.applyPlaceholdersFromPingAddress(
+                                            command, address
+                                    )
+                            )
+            );
+        }
+    }
+}
