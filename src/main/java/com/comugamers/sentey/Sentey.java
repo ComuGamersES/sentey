f1b7eda4fd1e6a25f7161f2beec0668d5ba65a1e
Jojo1542 <admin@jojo1542.es>
2022-06-23 22:56:11 -0300

    fix(plugin): remove java version check

--- src/main/java/com/comugamers/sentey/Sentey.java
+++ src/main/java/com/comugamers/sentey/Sentey.java
@@ -29,32 +29,6 @@ public class Sentey extends JavaPlugin {
 
     @Override
     public void onLoad() {
-        // Check if the server is running Java 16 or later. We do this on plugin load
-        // to prevent Guice from doing its things before sending the warning to the logs.
-        //
-        // We're using the deprecated SystemUtils#getJavaVersion() method since the
-        // SystemUtils#isJavaVersionAtLeast() is the most buggy thing in existence
-        if (SystemUtils.getJavaVersion() >= 16) {
-            // If so, warn the server admin that this plugin may not be compatible with it
-            getLogger().warning(
-                    "It looks like you're running Java 16 or higher. " + this.getDescription().getName()
-                            + " may not be fully compatible with it."
-            );
-
-            // Get required arguments depending on the Java version being used.
-            // The reason we do this is that in Java 17 the '--illegal-access=permit' argument was removed.
-            String requiredArguments = SystemUtils.getJavaVersion() >= 17
-                    ? "--add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util.zip=ALL-UNNAMED"
-                    : "--illegal-access=permit";
-
-            // And tell the admin to add the following arguments to their
-            // JVM in order to solve the errors during plugin initialization
-            getLogger().warning(
-                    "Please make sure to include the following argument(s) to your JVM - especially if" +
-                            " you see errors during plugin initialization: \"" + requiredArguments + "\""
-            );
-        }
-
         // Initialize array lists
         this.loginFilters = new ArrayList<>();
         this.loginActions = new ArrayList<>();
