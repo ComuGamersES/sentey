8c6866e13ff59245f70dc594192345896ecd041b
Jojo1542 <admin@jojo1542.es>
2022-06-25 22:32:37 -0300

    docs(util): add javadocs to IntegerUtil#isInteger()

--- src/main/java/com/comugamers/sentey/util/IntegerUtil.java
+++ src/main/java/com/comugamers/sentey/util/IntegerUtil.java
@@ -2,6 +2,11 @@ package com.comugamers.sentey.util;
 
 public class IntegerUtil {
 
+    /**
+     * Checks if a {@link String} is a valid {@link Integer int}.
+     * @param str The {@link String} to check.
+     * @return true if the provided value is a valid {@link Integer}, false if not.
+     */
     public static boolean isInteger(String str) {
         try {
             Integer.parseInt(str);
