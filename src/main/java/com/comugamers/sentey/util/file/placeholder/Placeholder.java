48b339cbf0a9654ff4acbc60c72679c08fab647d
Pabszito <pabszito@gmail.com>
2022-07-12 08:55:36 -0300

    refactor(placeholder): bye bye 400 constructors

--- src/main/java/com/comugamers/sentey/util/file/placeholder/Placeholder.java
+++ src/main/java/com/comugamers/sentey/util/file/placeholder/Placeholder.java
@@ -5,54 +5,9 @@ public class Placeholder {
     private final String value;
     private final String replacement;
 
-    public Placeholder(String value, String replacement) {
-        this.value = value;
-        this.replacement = replacement;
-    }
-
-    public Placeholder(String value, int replacement) {
-        this.value = value;
-        this.replacement = String.valueOf(replacement);
-    }
-
-    public Placeholder(String value, double replacement) {
-        this.value = value;
-        this.replacement = String.valueOf(replacement);
-    }
-
-    public Placeholder(String value, float replacement) {
-        this.value = value;
-        this.replacement = String.valueOf(replacement);
-    }
-
-    public Placeholder(String value, long replacement) {
-        this.value = value;
-        this.replacement = String.valueOf(replacement);
-    }
-
-    public Placeholder(String value, short replacement) {
-        this.value = value;
-        this.replacement = String.valueOf(replacement);
-    }
-
-    public Placeholder(String value, byte replacement) {
-        this.value = value;
-        this.replacement = String.valueOf(replacement);
-    }
-
-    public Placeholder(String value, char replacement) {
-        this.value = value;
-        this.replacement = String.valueOf(replacement);
-    }
-
-    public Placeholder(String value, boolean replacement) {
-        this.value = value;
-        this.replacement = String.valueOf(replacement);
-    }
-
     public Placeholder(String value, Object object) {
         this.value = value;
-        this.replacement = object.toString();
+        this.replacement = String.valueOf(object);
     }
 
     public String getValue() {
